af3f823d64556439af445b0588fd85342a535442
gerald <geraldbaalham@live.co.uk>
2024-10-25 11:20:38 +0100

    disabled router posts

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -11,20 +11,20 @@ export const postRouter = createTRPCRouter({
       };
     }),
 
-  create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      return ctx.db.post.create({
-        data: {
-          name: input.name,
-        },
-      });
-    }),
+  // create: publicProcedure
+  //   .input(z.object({ name: z.string().min(1) }))
+  //   .mutation(async ({ ctx, input }) => {
+  //     return ctx.db.post.create({
+  //       data: {
+  //         name: input.name,
+  //       },
+  //     });
+  //   }),
 
-  getLatest: publicProcedure.query(async ({ ctx }) => {
-    const post = await ctx.db.post.findFirst({
-      orderBy: { createdAt: "desc" },
-    });
+  // getLatest: publicProcedure.query(async ({ ctx }) => {
+  //   const post = await ctx.db.post.findFirst({
+  //     orderBy: { createdAt: "desc" },
+  //   });
 
     return post ?? null;
   }),
